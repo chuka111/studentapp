86d128fae8c2b374bf8dc16f643af03f2fbfb168
chuka <g00414637@atu.ie>
2023-11-01 22:21:07 +0000

    added a class to the project

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -1,4 +1,31 @@
 package ie.atu;
 
 public class StudentApp {
+    String studentName;
+    String studentEmail;
+    String studentCourse;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    public String getStudentCourse() {
+        return studentCourse;
+    }
+
+    public void setStudentCourse(String studentCourse) {
+        this.studentCourse = studentCourse;
+    }
 }
